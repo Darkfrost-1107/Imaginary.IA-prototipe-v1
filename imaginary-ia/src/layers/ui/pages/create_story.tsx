ecc8ad19beb41381236e33ae535468434afca7f0
Gabriel-Bernedo <132683380+Gabriel-Bernedo@users.noreply.github.com>
2024-10-21 23:33:17 -0500

    implementing Functionality to Front-end

--- imaginary-ia/src/layers/ui/pages/create_story.tsx
+++ imaginary-ia/src/layers/ui/pages/create_story.tsx
@@ -8,11 +8,11 @@ import { TextArea_Input } from '../inputs/textarea_input'
 import { Select_Input } from '../inputs/select_input'
 import { Range_Input } from '../inputs/range_input'
 import { Text_Container } from '../containers/text_container'
+import { Story_Creator_Component } from '../components/story_creator_component'
 interface PageProps {
   //Props
 }
 
-const Topics_List = ["Aventura", "Fantasía", "Ciencia Ficción", "Terror", "Comedia"]
 
 export const Create_Story_Page : FC<PageProps> = () => {
   return (
@@ -24,35 +24,8 @@ export const Create_Story_Page : FC<PageProps> = () => {
         <Subtitle_Container title="Generador de cuentos para niños"/>
       </Title_Layout>
 
-      <Grid_Layout>
-        <Panel_Layout>
-          
-          <Title_Container title="Contenido General"/>
-          <Padding_Layout>
-            <Subtitle_Container title="Descripción del Cuento"/>
-            <TextArea_Input/>
-          </Padding_Layout>
-
-          <Padding_Layout>
-            <Subtitle_Container title="Temática del Cuento"/>
-            <Select_Input options={Topics_List}/>
-          </Padding_Layout>
-
-        </Panel_Layout>
-
-        <Panel_Layout>
-          <Title_Container title="Otras configuraciones"/>
-          <Padding_Layout>
-            <Subtitle_Container title="Tiempo de Lectura"/>
-            <Text_Container text="Cantidad de Escenas" />
-            <Range_Input/>
-
-            <Text_Container text="Cantidad de Palabras por Escena" />
-            <Range_Input/>
-          </Padding_Layout>
-
-        </Panel_Layout>
-      </Grid_Layout>
+      <Story_Creator_Component/>
+      
     </div>
   )
 }
