2ddb94ab888449e0d954d4079cdcfa1fff14e835
Navi <ihuayhuam@unsa.edu.pe>
2024-10-22 07:06:55 -0500

    range style

--- imaginary-ia/src/layers/ui/inputs/range_input.tsx
+++ imaginary-ia/src/layers/ui/inputs/range_input.tsx
@@ -9,6 +9,13 @@ interface Range_Input_Props extends Input_Props {
 export const Range_Input : FC<Range_Input_Props> = ({className}) => {
   className = className || ""
   return (
+    <div class="p-16">
+    <div class="flex justify-between w-full">
+        <div>0</div>
+        <div>10</div>
+    </div>
+    <input type="range" class="h-2 w-full cursor-ew-resize appearance-none rounded-full bg-gray-200 disabled:cursor-not-allowed">
+    </div>
     <Input_Base className={className} props={{
       type:"range"
     }} />
