b7c3d0de91f180f9c8ff312cb6c820c3321de81c
Navi <ihuayhuam@unsa.edu.pe>
2024-10-22 08:22:17 -0500

    checkbox

--- imaginary-ia/src/layers/ui/inputs/checkbox_input.tsx
+++ imaginary-ia/src/layers/ui/inputs/checkbox_input.tsx
@@ -1,21 +1,21 @@
-import {FC} from 'react'
-import { Input_Props } from './types'
-import { Input_Base } from './input_base'
+import { FC } from 'react';
+import { Input_Props } from './types';
+import { Input_Base } from './input_base';
 
 interface CheckBox_Input_Props extends Input_Props {
-  //Props
-  label: string
+  label: string;
 }
 
-export const CheckBox_Input : FC<CheckBox_Input_Props> = ({className, label}) => {
-  className = className || ""
+export const CheckBox_Input: FC<CheckBox_Input_Props> = ({ className, label }) => {
+  className = className || '';
   let props = {
-    type: "checkbox",
-  }
+    type: 'checkbox',
+  };
+
   return (
-    <label>
-      <Input_Base props={props} className={className}/>
-      {label}  
-    </ label>
-  )
-}
+    <div className={`${className}`}>
+      <Input_Base props={props} className="w-4 h-4" />
+      <label className="text-white">{label}</label>
+    </div>
+  );
+};
