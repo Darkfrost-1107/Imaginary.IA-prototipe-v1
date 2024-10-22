5c1aae44a09a4d6ce027dabf192ce81bb9e9a644
Navi <ihuayhuam@unsa.edu.pe>
2024-10-22 07:38:20 -0500

    count

--- imaginary-ia/src/layers/ui/inputs/range_input.tsx
+++ imaginary-ia/src/layers/ui/inputs/range_input.tsx
@@ -1,31 +1,44 @@
-import {FC} from 'react'
+import { FC, useState } from 'react'
 import { Input_Props } from './types'
 import { Input_Base } from './input_base'
 import { Text_Container } from '../containers/text_container'
 
 interface Range_Input_Props extends Input_Props {
   //Props
-  range: Range_Interval
 }
 
-type Range_Interval = {
-  min: number
-  max: number
-}
+export const Range_Input: FC<Range_Input_Props> = ({ className }) => {
+  const [currentValue, setCurrentValue] = useState(5);  
 
-export const Range_Input : FC<Range_Input_Props> = ({className, range}) => {
   className = className || "h-2 w-full cursor-ew-resize appearance-none rounded-full bg-gray-200 disabled:cursor-not-allowed"
-  
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value);
+    setCurrentValue(newValue);
+  };
+
   return (
     <div className="p-8">
-      <Input_Base className={className} props={{
-      type:"range"
-      }} />
-    <div className="pt-2 flex justify-between w-full">
-        <Text_Container text="0" />
-        <Text_Container text="10" />
+      <div className="flex items-center gap-8">
+        <div className="flex-1">
+          <Input_Base 
+            props={{
+              type: "range",
+              min: "0",
+              max: "10",
+              value: currentValue,
+              onChange: handleChange
+            }} 
+          />
+          <div className="pt-2 flex justify-between w-full">
+            <Text_Container text="0" />
+            <Text_Container text="10" />
+          </div>
+        </div>
+        <div className="bg-blue-500 text-white rounded-full h-12 w-12 flex items-center justify-center text-lg font-semibold shadow-lg">
+          {currentValue}
+        </div>
+      </div>
     </div>
-    </div>
-    
-  )
+  );
 }
